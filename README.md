1bc5967493c45ffa8831541ce62da227d48a89ff
PaulaBSilva <113553913+undefined@users.noreply.github.com>
2022-09-19 19:51:04 +0000

    First commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# lwcatividade2
-Created with WebComponents.dev
---
+++ src/app.js
@@ -0,0 +1,12 @@
+import { LightningElement } from "lwc";
+
+export default class App extends LightningElement {
+ 
+ Lead = {
+ idade : "25",
+ email : "paulabsilva25@gmail.com",
+ classi : "Hot",
+ empre : "Jujuba",
+ recan : "200.000"
+};
+}
